c11684e2fb706537164c06adf458e8146f88e7dd
Adrien Detailleur <adriend@kickmaker.net>
2025-09-01 13:56:31 +0200

    feat: keyboard support for toggling

--- script.js
+++ script.js
@@ -9,17 +9,27 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             const li = document.createElement("li");
             li.textContent = element.value.trim();
+            li.tabIndex = "0";
             todolist.appendChild(li);
         }
         element.value = "";
         element.focus();
     }
 
-    function elementDone(event) {
+    function clickElementDone(event) {
         selectedElement = event.target.closest('li');
         selectedElement.classList.toggle("done");
     }
 
+    function keydownElementDone(event) {
+        if (event.code == " " || event.code == "Enter")
+        {
+            selectedElement = event.target.closest('li');
+            selectedElement.classList.toggle("done");
+        }
+    }
+
     form.addEventListener("submit", addElementInList);
-    todolist.addEventListener("click", elementDone);
+    todolist.addEventListener("click", clickElementDone);
+    todolist.addEventListener("keydown", keydownElementDone);
 });
